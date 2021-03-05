240aca86ab10edae5d8b89041c099a3d47054fae
miyaseinto <cbj2877@yahoo.co.jp>
2021-03-05 22:28:09 +0900

    s3setting

--- laravel/app/Http/Controllers/TweetController.php
+++ laravel/app/Http/Controllers/TweetController.php
@@ -88,8 +88,9 @@ class TweetController extends Controller
                 $tag_view = '画像以外のファイルが指定されています。画像ファイル(png/jpg/jpeg/gif)を指定して下さい';
                 return view('tweets.tag', compact('tag_view'));
             }
-            $path = Storage::disk('s3')->putFile('myprefix', $filename, 'public');
+            $path = Storage::disk('s3')->put('myprefix', $filename, 'public');
             $tweet->image = Storage::disk('s3')->url($path);
+
         }
 
         preg_match_all('/#([a-zA-Z0-9０-９ぁ-んァ-ヶー一-龠]+)/u', $request->tag_box, $match);
